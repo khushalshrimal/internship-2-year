859a7dbdf3d03d78a17b2bc6569e7d0892b93eca
khushalshrimal <khushalshrimal@gmail.com>
2026-05-22 10:03:13 +0530

    assignment

---
+++ ass3q1.py
@@ -0,0 +1,18 @@
+student = {"name": "Khushal","age": 20,"course": "CSE"}
+print("Dictionary:")
+print(student)
+print(student["name"])
+student["city"] = "Jaipur"
+print(student)
+
+fruits = ("apple", "banana", "mango")
+print("Tuple:")
+print(fruits)
+print(fruits[0])
+
+numbers = {1, 2, 3, 4, 4, 5}
+print("Set:")
+print(numbers)
+numbers.add(6)
+print("After adding 6:")
+print(numbers)
---
+++ ass3q2.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero is not allowed."
+    else:
+        return a / b
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print("Addition =", add(num1, num2))
+print("Subtraction =", subtract(num1, num2))
+print("Multiplication =", multiply(num1, num2))
+print("Division =", divide(num1, num2))
